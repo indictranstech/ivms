5f26f0183322d84d3a8bfdb91dc8bf6c83a889d3
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-05-04 13:09:27 +0530

    Adding additional validation and form changes

--- ivms/ivms/doctype/voucher/voucher.js
+++ ivms/ivms/doctype/voucher/voucher.js
@@ -1,4 +1,10 @@
 // Copyright (c) 2016, New Indictrans Technologies PVT LTD and contributors
 // For license information, please see license.txt
 
-
+frappe.ui.form.on("Voucher", {
+    refresh: function(frm) {
+        // use the __islocal value of doc, to check if the doc is saved or not
+        frm.set_df_property("advance_amount", "read_only", frm.doc.__islocal ? 0 : 1)
+        frm.set_df_property("prepaid_amount", "read_only", frm.doc.__islocal ? 0 : 1)
+    }
+});
