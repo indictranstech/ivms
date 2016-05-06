237197176164dcee8ce0aeb4f13dda995ca08ff4
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-05-06 12:15:40 +0530

    Removed whitespaces

--- ivms/api.py
+++ ivms/api.py
@@ -4,11 +4,9 @@ import frappe
 @frappe.whitelist()
 def get_manager_name(manager):
 	manager=frappe.get_doc("User",manager)
-	full_name=manager.full_name
-	return full_name 
+	return manager.full_name 
 
 @frappe.whitelist()
 def get_accounts_officer_name(accounts_officer):
 	accounts_officer=frappe.get_doc("User",accounts_officer)
-	full_name=accounts_officer.full_name
-	return full_name 
+	return accounts_officer.full_name
--- ivms/ivms/doctype/voucher/voucher.js
+++ ivms/ivms/doctype/voucher/voucher.js
@@ -10,53 +10,38 @@ frappe.ui.form.on("Voucher", {
 });
 
 cur_frm.fields_dict.manager.get_query = function(doc){
-	
 	return {
 		query:"ivms.ivms.doctype.voucher.voucher.get_manager_list"
 	}
 }
 cur_frm.fields_dict.accounts_officer.get_query = function(doc){
-	
 	return {
 		query:"ivms.ivms.doctype.voucher.voucher.get_accounts_officer_list"
 	}
 }
 frappe.ui.form.on("Voucher","manager" ,function(frm){
-	
 	return frappe.call({
 			method: "ivms.api.get_manager_name",
 			args: {
-				manager: frm.doc.manager,
-				
+				manager: frm.doc.manager,	
 			},
 			callback: function(r) {
 				if(r.message){
-	
 					frm.set_value("manager_name", r.message);
-			
-
-					}
-				
-			}
+			}	
 		});
-	})
+})
 frappe.ui.form.on("Voucher","accounts_officer" ,function(frm){
-	
 	return frappe.call({
 			method: "ivms.api.get_accounts_officer_name",
 			args: {
 				accounts_officer: frm.doc.accounts_officer,
-				
 			},
 			callback: function(r) {
 				if(r.message){
-	
 					frm.set_value("accounts_officer_name", r.message);
-			
-
-					}
-				
+				}
 			}
 		});
-	})
+})
 
